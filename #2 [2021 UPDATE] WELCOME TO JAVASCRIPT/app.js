fb8ccd157586597998078a9f7ae85835b8d1a3d3
Ji Hye <lovinsurfaces@gmail.com>
2021-07-05 21:01:37 +0900

    add app.js

---
+++ #2 [2021 UPDATE] WELCOME TO JAVASCRIPT/app.js
@@ -0,0 +1,23 @@
+const calculator = {
+    "plus": function(a, b) {
+        console.log(a + b);
+    },
+    "minus": function(a, b) {
+        console.log(a - b);
+    },
+    "divide": function(a, b) {
+        console.log(a / b);
+    },
+    "multiply": function(a, b) {
+        console.log(a * b);
+    },
+    "square": function(a) {
+        console.log(a * a);
+    } 
+}
+
+calculator.plus(1, 2); // 3
+calculator.minus(5, 3); // 2
+calculator.divide(5, 10); // 2
+calculator.multiply(3, 4); // 12
+calculator.square(5); // 25
