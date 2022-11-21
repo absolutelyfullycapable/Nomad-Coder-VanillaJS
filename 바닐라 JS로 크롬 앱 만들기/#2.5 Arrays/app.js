0195c405418fea6292e4f85ecf0d2770816bc522
JIHYE <lovinsurfaces@gmail.com>
2022-11-21 11:11:19 +0900

    Add files via upload

--- 바닐라 JS로 크롬 앱 만들기/#2.5 Arrays/app.js
+++ 바닐라 JS로 크롬 앱 만들기/#2.5 Arrays/app.js
@@ -32,12 +32,16 @@ const daysOfWeek2 = ["mon", "tue", "wed", "thu", "fri", "sat"];
 // Q. 한 주의 요일 중 5번째는 뭔가요? = 배열의 다섯 번째 요소는 무엇인가요?
 console.log(daysOfWeek2[4]);
 
-/* 배열 안의 데이터에 접근하고 싶다면?
+/*
+  배열 안의 데이터에 접근하고 싶다면?
   => 변수의 이름을 적어 주고 대괄호를 열어 우리가 얻고 싶은 항목의 번호를 넣고 대괄호를 닫아 주면 됨
   => 컴퓨터는 1부터 숫자를 세지 않고 0부터 세기 시작한다는 점을 유의하기 (= 인덱스는 0부터 시작)
 */
 
-// Add one more day to the array
+// Get Item from Array
 console.log(daysOfWeek2); // ["mon", "tue", "wed", "thu", "fri", "sat"];
+
+// Add one more day to the array
 daysOfWeek2.push("sun");
-console.log(daysOfWeek2); //["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+console.log(daysOfWeek2); // ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
