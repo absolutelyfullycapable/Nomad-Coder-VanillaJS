79ee6598adc93c31a1dab92df9c6f0cc1b51b8c9
zizi <lovinsurfaces@gmail.com>
2023-06-20 17:47:49 +0900

    강의 내용 추가

--- 바닐라 JS로 크롬 앱 만들기/#3.5 More Events/app.js
+++ 바닐라 JS로 크롬 앱 만들기/#3.5 More Events/app.js
@@ -6,6 +6,10 @@ Event를 사용하는 방법
 대부분 element 찾아서 -> event listener 추가하고 -> event가 발생하면 반응해 주는 패턴
 
 * 강의 내에서는 추후 removeEventListener를 통해 event listener를 제거할 수 있어 (1) 방법을 더 선호함
+
+document의 body,head,title 이런것들은 중요하기 때문에
+document.body.style~의 명령이 허용되지만 div 같은 elements는 호출이 안 됨
+나머지 element들은 querySelector나 getElementById로 불러와야 됨
 */
 
 const h1 = document.querySelector("div.hello:first-child h1");
